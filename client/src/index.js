034011633fea6ca37d905d59c30de3416c64e09e
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2022-04-23 16:09:39 +0700

    feat: added handler for data received from form

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function SideBars(title){
@@ -40,54 +41,90 @@ class TambahPenyakit extends React.Component{
   }
 }
 
-class TesDNA extends React.Component{
-  // constructor(props){
-  //   super(props);
-  //   this.state = {message: ""};
-  // }
-  render(){
-    return(
-      <div>
+function DNAForm() {
+  const {
+    register, 
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    const reader = new FileReader();
+    console.log(data)
+    reader.readAsText(data.DNAsequence[0]);
+    reader.onload = function(e) {
+      var rawLog = reader.result;
+      console.log(rawLog);
+    }
+  }
+  return (
+    <div>
         {SideBars("Tes DNA")} 
         <div className = "form">
           <Form>
             <Form.Group className="mb-5">
               <Form.Label>Nama Pengguna:</Form.Label>
-              <Form.Control type="text" placeholder="Masukkan nama pengguna"></Form.Control>
+              <Form.Control required type="text" placeholder="Masukkan nama pengguna" {...register("namaPengguna")}></Form.Control>
               <Form.Label className="mt-3">Prediksi Penyakit:</Form.Label>
-              <Form.Control type="text" placeholder="Masukkan prediksi penyakit"></Form.Control>
+              <Form.Control type="text" placeholder="Masukkan prediksi penyakit" {...register("prediksiPenyakit")}></Form.Control>
               <Form.Label className="mt-3">Pilih algoritma string matching:</Form.Label>
-              <Form.Select>
+              <Form.Select {...register("algoritma")}>
                 <option value="KMP">KMP</option>
                 <option value="Boyer-Moore">Boyer-Moore</option>
               </Form.Select>
               <Form.Label className="mt-3">Unggah file teks rantai DNA:</Form.Label>
-              <Form.Control type="file"></Form.Control>
-              <Button className="mt-3" type="button">Submit</Button>
+              <Form.Control type="file" {...register("DNAsequence")}></Form.Control>
+              <Button className="mt-3" type="button" onClick = {handleSubmit(onSubmit)}>Submit</Button>
             </Form.Group>  
           </Form>
         </div>
       </div>
-
-      // <div>
-      //   {SideBars("Tes DNA")}
-      //   <button className='helloWorld' onClick={() => this.CallBackend()}>
-      //     {"Hello World!"}
-      //   </button>
-      //   <div className='result'>
-      //     {this.state.message}
-      //   </div>
-      // </div>
-    );
-  }
+  )
+};
   
-  async CallBackend(){
-    let Response = await fetch("http://localhost:3001/helloWorld").catch(err => err);
-    let Text = await Response.text();
-    this.setState({message: Text});
 
-  }
-}
+// class TesDNA extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         {SideBars("Tes DNA")} 
+//         <div className = "form">
+//           <Form>
+//             <Form.Group className="mb-5">
+//               <Form.Label>Nama Pengguna:</Form.Label>
+//               <Form.Control required type="text" name="namaPengguna" placeholder="Masukkan nama pengguna" ref={register()}></Form.Control>
+//               <Form.Label className="mt-3">Prediksi Penyakit:</Form.Label>
+//               <Form.Control type="text" name="prediksiPenyakit" placeholder="Masukkan prediksi penyakit" ref={register()}></Form.Control>
+//               <Form.Label className="mt-3">Pilih algoritma string matching:</Form.Label>
+//               <Form.Select name="algoritma" ref={register()}>
+//                 <option value="KMP">KMP</option>
+//                 <option value="Boyer-Moore">Boyer-Moore</option>
+//               </Form.Select>
+//               <Form.Label className="mt-3">Unggah file teks rantai DNA:</Form.Label>
+//               <Form.Control type="file" name="teksDNA" ref={register()}></Form.Control>
+//               <Button className="mt-3" type="button" onClick = {handleSubmit(onSubmit)}>Submit</Button>
+//             </Form.Group>  
+//           </Form>
+//         </div>
+//       </div>
+
+//       // <div>
+//       //   {SideBars("Tes DNA")}
+//       //   <button className='helloWorld' onClick={() => this.CallBackend()}>
+//       //     {"Hello World!"}
+//       //   </button>
+//       //   <div className='result'>
+//       //     {this.state.message}
+//       //   </div>
+//       // </div>
+//     );
+//   }
+  
+//   async CallBackend(){
+//     let Response = await fetch("http://localhost:3001/helloWorld").catch(err => err);
+//     let Text = await Response.text();
+//     this.setState({message: Text});
+
+//   }
+// }
 
 class RiwayatTes extends React.Component{
   render(){
@@ -104,7 +141,7 @@ ReactDOM.render (
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/tambahPenyakit' element={<TambahPenyakit/>}></Route>
-      <Route path='/tesDNA' element={<TesDNA/>}></Route>
+      <Route path='/tesDNA' element={<DNAForm/>}></Route>
       <Route path='/riwayatTes' element={<RiwayatTes/>}></Route>
     </Routes>
   </BrowserRouter>,
